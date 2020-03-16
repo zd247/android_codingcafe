664051a5c15f1fddbf6dcf950d79f513284ee297
zd247 <zduy2407@gmail.com>
2020-03-16 15:18:20 +0700

    egg timer

---
+++ Instagram Clone/app/src/main/java/rattclub/c/instagramclone/AddPostActivity.kt
@@ -0,0 +1,12 @@
+package rattclub.c.instagramclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddPostActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_post)
+    }
+}
